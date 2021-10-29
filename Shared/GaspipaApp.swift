c6966640b4999e94a91e5cdf5d0ffcf4c465ddc1
norm <n>
2021-10-29 10:29:59 +0200

    Initial Commit

---
+++ Shared/GaspipaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaspipaApp.swift
+//  Shared
+//
+//  Created by Etudiant on 29/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct GaspipaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
